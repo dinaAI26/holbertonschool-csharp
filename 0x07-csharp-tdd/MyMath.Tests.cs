eb901d1234995e948726afa3c739cfe4df28daaa
dinaAI26 <87501934+dinaAI26@users.noreply.github.com>
2021-07-30 22:27:43 +0300

    Create MyMath.Tests.cs

---
+++ 0x07-csharp-tdd/MyMath.Tests.cs
@@ -0,0 +1,18 @@
+using system;
+class operations
+public static int Add(int a, int b)
+{
+    int total;
+    total= a + b;
+    return total;
+}
+public static void Main()
+{
+    Console.Write('Enter two numbers:');
+    int a= Convert.To
+Int32(Console.ReadLine());
+    int b= Convert.To 
+Int32(Console.ReadLine())
+    Console.WriteLine("\nThe sum of the two numbers is : {0} \n, Sum(a,b))");
+    Console.ReadLine();
+}
