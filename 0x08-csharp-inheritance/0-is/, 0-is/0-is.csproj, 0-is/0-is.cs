144b33041e577d11447a77dd62bb904f9b63e25b
dinaAI26 <87501934+dinaAI26@users.noreply.github.com>
2021-08-02 23:32:28 +0300

    Create 0-is.cs

---
+++ 0x08-csharp-inheritance/0-is/, 0-is/0-is.csproj, 0-is/0-is.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = Console.WriteLine();
+        if (obj.IsOfTypeInt(a))
+        Console.WriteLine("True");
+        else
+        Console.WriteLine("False")
+    }
+    
+}
